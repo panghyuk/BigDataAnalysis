c103738bf21616f03233eb0a2482eb51d9a8abb7
panghyuk <okh9712@gmail.com>
2022-06-24 14:23:07 +0900

    README 수정

--- 캐글 연습문제/0617/T1-13.py
+++ 캐글 연습문제/0617/T1-13.py
@@ -6,8 +6,8 @@ df_corr = df.corr()
 # print(df_corr)
 df_corr = df_corr[:-1]
 
-corr_max = df_corr['quality'].max()
-corr_min = df_corr['quality'].min()
+corr_max = abs(df_corr['quality']).max()
+corr_min = abs(df_corr['quality']).min()
 # print(corr_max,corr_min)
 
 ans = corr_max - corr_min
